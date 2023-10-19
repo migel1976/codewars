c6c2f0ab5f35ff5c380a3c1b60e3a069f8a29097
smirnov <smirnov@alfasatcom.ru>
2023-10-19 15:26:57 +0300

    doubleinteger

---
+++ 8kyu/doubleinteger.js
@@ -0,0 +1,13 @@
+//DESCRIPTION
+// Code as fast as you can! You need to double the integer and return it.
+//
+// SOLUTION
+
+function doubleInteger(i){
+	return i*i
+}
+
+let i=process.argv[2]
+console.log(doubleInteger(i))
+
+
